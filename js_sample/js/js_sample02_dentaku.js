2feecd8c9079e56422fc4325f894438fcd0dd005
本田　有人 <>
2023-07-12 07:32:12 +0900

    電卓、キーボード制作。

---
+++ js_sample/js/js_sample02_dentaku.js
@@ -0,0 +1,9 @@
+const num1 = document.getElementsByClassName("num1");
+const num2 = document.getElementsByClassName("num2");
+const ans = document.getElementsByClassName("ans");
+const btn = document.getElementById("equ");
+const cul = document.getElementById("cul");
+function typed(e) {
+    console.log(e.target.value);
+}
+function typed(e)
